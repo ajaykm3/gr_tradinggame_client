8ba3c0a28a6f217cf30e7f272e44480c3a8c2c75
Ajay Mittal <akm20@ic.ac.uk>
2026-06-29 22:00:46 +0100

    Change server address in Client initialization
    Updated server address for the Client initialization.

--- submit.py
+++ submit.py
@@ -3,6 +3,6 @@ from solution import play
 
 # Password is set on first submission and required for all future submissions.
 # Choose any password you like on your first submit. Don't forget it.
-client = Client('YourTeamName', password='YourPassword', server='...')
+client = Client('YourTeamName', password='YourPassword', server='ec2-3-254-122-192.eu-west-1.compute.amazonaws.com:5000')
 client.test(play)
 client.submit(play)
